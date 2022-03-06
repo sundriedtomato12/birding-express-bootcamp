5ce15bc059b813de8827d6f2d99db72fa1e72834
sundriedtomato12 <felicia.tanwp@gmail.com>
2022-03-07 00:11:44 +0800

    finished delete note function, added new table users for user authentication. users are now able to sign up, login, and log out.

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get('/note/:id', (request, response) => {
   console.log('request came in to get page of note id:');
   console.log(request.params.id);
   const id = [request.params.id];
-  sqlQuery = 'SELECT * FROM notes WHERE id=$1';
+  sqlQuery = 'SELECT * FROM notes WHERE id = $1';
   pool.query(sqlQuery, id, (error, result) => {
     if (error) {
       console.log('Error executing query', error.stack);
@@ -94,9 +94,9 @@ app.get('/note/:id', (request, response) => {
 // GET function to get page to edit note by id
 app.get('/note/:id/edit', (request, response) => {
   console.log('request came in to get page to edit note id:');
-  console.log(request.params.index);
+  console.log(request.params.id);
   const id = [request.params.id];
-  sqlQuery = 'SELECT * FROM notes WHERE id=$1';
+  sqlQuery = 'SELECT * FROM notes WHERE id = $1';
   pool.query(sqlQuery, id, (error, result) => {
     if (error) {
       console.log('Error executing query', error.stack);
@@ -107,23 +107,128 @@ app.get('/note/:id/edit', (request, response) => {
     if (result.rows.length <= 0) {
       console.log('No such id!');
     } else {
-      console.log(result.rows);
+      console.log(result.rows[0]);
+      const data = result.rows[0];
+      response.render('editnote', { data });
     }
-    const data = result.rows[0];
-    response.render('editnote', { data });
   });
 });
 
 // PUT function to edit note by id
 app.put('/note/:id/', (request, response) => {
   console.log('request came in to edit note id:');
-  console.log(request.params.index);
+  console.log(request.params.id);
+  const { observation_date, behaviour, flock_size } = request.body;
+  const inputData = [request.params.id, observation_date, behaviour, flock_size];
+
+  sqlQuery = 'UPDATE notes SET observation_date = $2, behaviour = $3, flock_size = $4 WHERE id = $1';
+
+  console.log('input data');
+  console.log(inputData);
+
+  pool.query(sqlQuery, inputData, (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.log('note edited!');
+    response.render('noteedited');
+  });
 });
 
 // DELETE function to delete note by id
-app.delete('/note/:id/delete', (request, response) => {
+app.delete('/note/:id', (request, response) => {
   console.log('request came in to delete note id:');
-  console.log(request.params.index);
+  console.log(request.params.id);
+  const id = [request.params.id];
+  sqlQuery = 'DELETE FROM notes WHERE id=$1';
+
+  pool.query(sqlQuery, id, (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.log('delete successful');
+    response.render('notedeleted');
+  });
+});
+
+// GET function to get page to sign up new user
+app.get('/signup', (request, response) => {
+  console.log('request came in to sign up as a new user');
+  response.render('signup');
+});
+
+// POST function to create new user
+app.post('/signup', (request, response) => {
+  console.log('accept post request to sign up new user');
+  sqlQuery = 'INSERT INTO users (email, password) VALUES ($1, $2)';
+  const { email, password } = request.body;
+  const inputData = [email, password];
+
+  pool.query(sqlQuery, inputData, (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.log('Successfully added new user to database');
+    response.render('signupsuccessful');
+  });
+});
+
+// GET function to get page for user to input login credentials
+app.get('/login', (request, response) => {
+  console.log('request came in for user to login');
+  response.render('login');
+});
+
+// POST function to accept user's request to log in
+app.post('/login', (request, response) => {
+  console.log('accept post request to log user in');
+  const inputData = [request.body.email];
+
+  sqlQuery = 'SELECT * FROM users WHERE email=$1';
+
+  pool.query(sqlQuery, inputData, (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send('sorry! there was an error!');
+      return;
+    }
+
+    if (result.rows.length === 0) {
+      // we didnt find a user with that email.
+      // the error for password and user are the same
+      // don't tell the user which error they got for security reasons,
+      // otherwise people can guess if a person is a user of a given service.
+      response.status(403).send('sorry!');
+      return;
+    }
+
+    const user = result.rows[0];
+    console.log(user);
+
+    if (user.password === request.body.password) {
+      response.cookie('loggedIn', true);
+      response.cookie('userId', user.id);
+      console.log('login successful!');
+      response.render('loginsuccessful');
+    } else {
+      response.status(403).send('sorry!');
+    }
+  });
+});
+
+// DELETE function to log user out
+app.delete('/logout', (request, response) => {
+  console.log('request came in to logout');
+  response.clearCookie('loggedIn');
+  response.clearCookie('userId');
+  console.log('successfully logged out');
+  response.render('logoutsuccessful');
 });
 
 app.set('view engine', 'ejs');
