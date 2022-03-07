6aac4ed83440937677f015f89eaeab26de0fe205
sundriedtomato12 <felicia.tanwp@gmail.com>
2022-03-07 22:53:27 +0800

    finished user authentication. users can now sign up, login and logout. app will also check if user is logged in through cookies and render logout button accordingly.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pg from 'pg';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
+import { format } from 'date-fns';
 
 // Initialise DB connection
 const { Pool } = pg;
@@ -27,6 +28,7 @@ let sqlQuery = '';
 app.get('/', (request, response) => {
   console.log('request came in to get home page with list of notes');
   sqlQuery = 'SELECT * from notes';
+  const loginResult = [];
   pool.query(sqlQuery, (error, result) => {
     if (error) {
       console.log('Error executing query', error.stack);
@@ -40,19 +42,39 @@ app.get('/', (request, response) => {
       console.log(result.rows);
     }
     const data = result.rows;
-    response.render('homepage', { data });
+    for (let i = 0; i < data.length; i += 1) {
+      const newDate = format(new Date(data[i].observation_date), 'dd MMM yyyy');
+      data[i].observation_date = newDate;
+    }
+    console.log(data);
+    console.log('Is user logged in?');
+    console.log(request.cookies.loggedIn);
+    if (request.cookies.loggedIn) {
+      loginResult.push('logged in');
+    } else {
+      loginResult.push('logged out');
+    }
+    console.log(loginResult);
+    response.render('homepage', { data, loginResult });
   });
 });
 
 // GET function to get page to create new note
 app.get('/note', (request, response) => {
   console.log('request came in to get form to create new note');
-  response.render('createnote');
+  const loginResult = [];
+  if (request.cookies.loggedIn) {
+    loginResult.push('logged in');
+  } else {
+    loginResult.push('logged out');
+  }
+  response.render('createnote', { loginResult });
 });
 
 // POST function post new note details into notes table
 app.post('/note', (request, response) => {
   console.log('accept post request to create new note');
+  const loginResult = [];
   sqlQuery = 'INSERT INTO notes (observation_date, behaviour, flock_size) VALUES ($1, $2, $3)';
   const { observation_date, behaviour, flock_size } = request.body;
   const inputData = [observation_date, behaviour, flock_size];
@@ -64,7 +86,12 @@ app.post('/note', (request, response) => {
       return;
     }
     console.log('Successfully executed query');
-    response.render('notecreated');
+    if (request.cookies.loggedIn) {
+      loginResult.push('logged in');
+    } else {
+      loginResult.push('logged out');
+    }
+    response.render('notecreated', { loginResult });
   });
 });
 
@@ -72,6 +99,7 @@ app.post('/note', (request, response) => {
 app.get('/note/:id', (request, response) => {
   console.log('request came in to get page of note id:');
   console.log(request.params.id);
+  const loginResult = [];
   const id = [request.params.id];
   sqlQuery = 'SELECT * FROM notes WHERE id = $1';
   pool.query(sqlQuery, id, (error, result) => {
@@ -87,7 +115,14 @@ app.get('/note/:id', (request, response) => {
       console.log(result.rows);
     }
     const data = result.rows[0];
-    response.render('viewnote', { data });
+    const newDate = format(new Date(data.observation_date), 'dd MMM yyyy');
+    data.observation_date = newDate;
+    if (request.cookies.loggedIn) {
+      loginResult.push('logged in');
+    } else {
+      loginResult.push('logged out');
+    }
+    response.render('viewnote', { data, loginResult });
   });
 });
 
@@ -95,6 +130,7 @@ app.get('/note/:id', (request, response) => {
 app.get('/note/:id/edit', (request, response) => {
   console.log('request came in to get page to edit note id:');
   console.log(request.params.id);
+  const loginResult = [];
   const id = [request.params.id];
   sqlQuery = 'SELECT * FROM notes WHERE id = $1';
   pool.query(sqlQuery, id, (error, result) => {
@@ -107,9 +143,16 @@ app.get('/note/:id/edit', (request, response) => {
     if (result.rows.length <= 0) {
       console.log('No such id!');
     } else {
-      console.log(result.rows[0]);
       const data = result.rows[0];
-      response.render('editnote', { data });
+      const newDate = format(new Date(data.observation_date), 'yyyy-MM-dd');
+      data.observation_date = newDate;
+      console.log(data);
+      if (request.cookies.loggedIn) {
+        loginResult.push('logged in');
+      } else {
+        loginResult.push('logged out');
+      }
+      response.render('editnote', { data, loginResult });
     }
   });
 });
@@ -118,6 +161,7 @@ app.get('/note/:id/edit', (request, response) => {
 app.put('/note/:id/', (request, response) => {
   console.log('request came in to edit note id:');
   console.log(request.params.id);
+  const loginResult = [];
   const { observation_date, behaviour, flock_size } = request.body;
   const inputData = [request.params.id, observation_date, behaviour, flock_size];
 
@@ -133,7 +177,12 @@ app.put('/note/:id/', (request, response) => {
       return;
     }
     console.log('note edited!');
-    response.render('noteedited');
+    if (request.cookies.loggedIn) {
+      loginResult.push('logged in');
+    } else {
+      loginResult.push('logged out');
+    }
+    response.render('noteedited', { loginResult });
   });
 });
 
@@ -141,6 +190,7 @@ app.put('/note/:id/', (request, response) => {
 app.delete('/note/:id', (request, response) => {
   console.log('request came in to delete note id:');
   console.log(request.params.id);
+  const loginResult = [];
   const id = [request.params.id];
   sqlQuery = 'DELETE FROM notes WHERE id=$1';
 
@@ -151,20 +201,32 @@ app.delete('/note/:id', (request, response) => {
       return;
     }
     console.log('delete successful');
-    response.render('notedeleted');
+    if (request.cookies.loggedIn) {
+      loginResult.push('logged in');
+    } else {
+      loginResult.push('logged out');
+    }
+    response.render('notedeleted', { loginResult });
   });
 });
 
 // GET function to get page to sign up new user
 app.get('/signup', (request, response) => {
   console.log('request came in to sign up as a new user');
-  response.render('signup');
+  const loginResult = [];
+  if (request.cookies.loggedIn) {
+    loginResult.push('logged in');
+  } else {
+    loginResult.push('logged out');
+  }
+  response.render('signup', { loginResult });
 });
 
 // POST function to create new user
 app.post('/signup', (request, response) => {
   console.log('accept post request to sign up new user');
   sqlQuery = 'INSERT INTO users (email, password) VALUES ($1, $2)';
+  const loginResult = [];
   const { email, password } = request.body;
   const inputData = [email, password];
 
@@ -175,19 +237,32 @@ app.post('/signup', (request, response) => {
       return;
     }
     console.log('Successfully added new user to database');
-    response.render('signupsuccessful');
+    if (request.cookies.loggedIn) {
+      loginResult.push('logged in');
+    } else {
+      loginResult.push('logged out');
+    }
+    response.render('signupsuccessful', { loginResult });
   });
 });
 
 // GET function to get page for user to input login credentials
 app.get('/login', (request, response) => {
   console.log('request came in for user to login');
-  response.render('login');
+  const loginResult = [];
+  if (request.cookies.loggedIn === true) {
+    loginResult.push('logged in');
+  } else {
+    loginResult.push('logged out');
+  }
+  console.log(loginResult);
+  response.render('login', { loginResult });
 });
 
 // POST function to accept user's request to log in
 app.post('/login', (request, response) => {
   console.log('accept post request to log user in');
+  const loginResult = [];
   const inputData = [request.body.email];
 
   sqlQuery = 'SELECT * FROM users WHERE email=$1';
@@ -215,7 +290,9 @@ app.post('/login', (request, response) => {
       response.cookie('loggedIn', true);
       response.cookie('userId', user.id);
       console.log('login successful!');
-      response.render('loginsuccessful');
+      loginResult.push('logged in');
+      console.log(loginResult);
+      response.render('loginsuccessful', { loginResult });
     } else {
       response.status(403).send('sorry!');
     }
@@ -224,11 +301,17 @@ app.post('/login', (request, response) => {
 
 // DELETE function to log user out
 app.delete('/logout', (request, response) => {
+  const loginResult = [];
   console.log('request came in to logout');
   response.clearCookie('loggedIn');
   response.clearCookie('userId');
   console.log('successfully logged out');
-  response.render('logoutsuccessful');
+  if (request.cookies.loggedIn === true) {
+    loginResult.push('logged in');
+  } else {
+    loginResult.push('logged out');
+  }
+  response.render('logoutsuccessful', { loginResult });
 });
 
 app.set('view engine', 'ejs');
